86ca8d2270555327d201009b44ef28c7337f9778
Eric Canas <elcorreodeharu@gmail.com>
2022-06-28 21:35:14 +0200

    Locale Language Support

--- UI/config.py
+++ UI/config.py
@@ -1,12 +1,28 @@
 import os
+import locale
+try:
+    import ctypes
+    windll = ctypes.windll.kernel32
+    loc_code = locale.windows_locale[windll.GetUserDefaultUILanguage()]
+except:
+    loc_code = locale.getdefaultlocale()[0]
+
 
 WINDOW_TITLE = "JPEG-MetaFilter"
-SELECT_FOLDER = "Folder containing JPEGs to filter"
-INVALID_DIR = "Select a valid directory"
-NO_IMAGES_IN_DIR = "No images found in the directory"
-SEARCH_BUTTON_TEXT_ANY = "Search files with ANY of these tags"
-SEARCH_BUTTON_TEXT_ALL = "Search files with ALL of these tags"
-FILTER_BY_TAGS_BUTTON_TEXT = "Filter by tags"
+if loc_code.lower().startswith('es_'):
+    SELECT_FOLDER = "Carpeta con los JPEGs a filtrar"
+    INVALID_DIR = "Debes seleccionar un directorio válido"
+    NO_IMAGES_IN_DIR = "Esta carpeta no contiene imagenes JPEG"
+    SEARCH_BUTTON_TEXT_ANY = "Imágenes con ALGUNA de estas etiquetas"
+    SEARCH_BUTTON_TEXT_ALL = "Imágenes con TODAS estas etiquetas"
+    FILTER_BY_TAGS_BUTTON_TEXT = "Filtrar por etiquetas"
+else:
+    SELECT_FOLDER = "Folder containing JPEGs to filter"
+    INVALID_DIR = "Select a valid directory"
+    NO_IMAGES_IN_DIR = "No JPEG images found in the directory"
+    SEARCH_BUTTON_TEXT_ANY = "Search files with ANY of these tags"
+    SEARCH_BUTTON_TEXT_ALL = "Search files with ALL of these tags"
+    FILTER_BY_TAGS_BUTTON_TEXT = "Filter by tags"
 
 TMP_DIR = ".tmp_JPEG-MetaFilter"
 
